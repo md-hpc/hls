2306ecc3472471dabf5dbf794b2b469a33de5509
vraiti <vraiti@redhat.com>
2024-10-30 11:56:42 -0400

    misc

--- phase1.py
+++ phase1.py
@@ -11,7 +11,7 @@ from structures import *
 '''
 Required IO. Used for communication with emulator.ControlUnit
 
-Override these values at the end of the script with references to your control units' IO attributes, e.g.
+Override these values at the end of the script with references to your control units IO attributes, e.g.
 
 class ReadController(Logic):
     def __init__(self):
@@ -35,4 +35,78 @@ CTL_DOUBLE_BUFFER = Input
 # Force evaluation control
 CTL_FORCE_EVALUATION_READY = Input # reads as 1 when force evaluation should begin/continue, otherwise 0
 CTL_FORCE_EVALUATION_DONE = Output # write 1 when your units are done evaluating forces, otherwise write 0
+
+def PositionReadController(Logic):
+    def __init__(self):
+        super().__init__()
+        
+        self.ctl_force_evaluation_ready = Input(self)
+        self.ctl_double_buffer = Input(self)
+
+        self.ctl_force_evaluation_done = Output(self)
+
+        self._next_timestep = True
+
+        self._cell_r = 0
+        self._addr_r = 0
+        self._addr_n = 0
+
+        # read from register that PositionReader writes
+        self.stale_reference = Input(self) # if all particles read last cycle were NULL
+
+        self.oaddr = Output(self)
+        self.cell_r = Output(self)
+
+        self._new_reference = False
+        self.new_reference = Output(self) # if PositionReader should write a new value to the reference register
+        
+
+    def halt(self):
+        self.next_timestep = True
+        self.ctl_force_evaluation_done.set(1)
+        self.oaddr.set(NULL)
+        self.cell_r.set(NULL)
+        self.new_reference.set(NULL)
+
+    def logic(self):
+        ctl_force_evaluation_ready = self.ctl_force_evaluation_ready.get() 
+        if ctl_force_evaluation_ready and self._cell_r == N_CELL:
+            self.halt()
+
+        if not ctl_force_evaluation_ready:
+            self._cell_r = 0
+            self._addr_r = 0
+            self._addr_n = 0
+            self.next_timestep = True
+            self.halt()
+
+        ctl_double_buffer = self.ctl_double_buffer.get()
+        stale_reference = self.stale_reference.get()
+
+        offset = 256 if ctl_double_buffer else 0
+
+        if self.next_timestep:
+            self.next_timestep = False
+            self._new_reference = False
+            stale_reference = True
+
+        if stale_reference:
+            if self._new_reference:
+                self._cell_r += 1
+                self._addr_r = 0
+            else:
+                self._addr_r += 1
+                self._new_reference = True
+            if self._cell_r == N_CELL:
+                self.halt()
+                return
+            addr = self._addr_r
+        else:
+            addr = self._addr_n
+        
+        self.new_reference.set(self._new_reference)
+        self.cell_r.set(self._cell_r)
+        self.oaddr.set(addr)
+        self.ctl_force_evaluation_done.set(0)
+
 
